e1f341b3ddd5001d1563853dfa05ac91cabd93cd
Sfoucheur <sebastien.foucheur@bretagne.bzh>
2020-04-15 15:32:50 +0200

    add svg start

--- script.js
+++ script.js
@@ -1,83 +1,39 @@
 
 $(document).ready(function () {
-    // Init Map
-    var map = L.map('epci_map').setView([48.10743118848039, -2.8564453125000004], 8);
-    var osmLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        maxZoom: 19
-    });
-    map.addLayer(osmLayer);
-    L.Control.ResetSelected = L.Control.extend({
-        onAdd: function(map) {
-            var btn = L.DomUtil.create('a');
-    
-            btn.textContent = 'Annuler la selection';
-            btn.classList.add('resetButton');
-    
-            return btn;
-        },
-    
-        onRemove: function(map) {
-            // Nothing to do here
+
+
+    var MAP_WIDTH  = 620;
+    var MAP_HEIGHT = 600;
+    var mapContainer = document.getElementById("epci_map");
+    var map = new Raphael(mapContainer, MAP_WIDTH, MAP_HEIGHT);
+    // Init Svg map
+    fetch('epci_simple.svg')
+    .then(response => response.text())
+    .then(epcis => {
+        for(var epciName in epcis) {
+            (function (epci) {
+                // region.attr(style);
+                // region.attr('title', regionName);
+        
+                // region[0].addEventListener("mouseover", function() {
+                //     region.animate(hoverStyle, animationSpeed);
+                // }, true);
+        
+                // region[0].addEventListener("mouseout", function() {
+                //   region.animate(style, animationSpeed);
+                // }, true);
+                
+                // region[0].addEventListener("click", function(e) {
+                //   var value = e.target.getElementsByTagName("title")[0].textContent;
+                //   document.getElementById("infos").textContent = value;
+                // }, true);
+                console.log("Nom : ",epciName," PATH : ",epci);
+        
+            })(epcis[epciName]);
         }
     });
+
     
-    L.Control.resetSelected = function(opts) {
-        return new L.Control.ResetSelected(opts);
-    }
-    L.Control.resetSelected({ position: 'topright' }).addTo(map);
-    // Bind event to custom control
-    $(".resetButton").on('click',function(){
-        $(".collapse").collapse("hide");
-        $("#myInput").val("");
-        $(".card").show();
-    });
-    // Get epci_simple geoJson
-    $.ajax({
-        url : "https://kartenn.region-bretagne.fr/kartoviz/apps/region/territoire/data/epci_simple.geojson",
-        success : function(data){
-            var hoverStyle = {
-                color :'red',
-                weight:3
-            }
-            var basicStyle = {
-                color :'rgba(45, 64,89,255)',
-                fillOpacity:0,
-                fillColor:'rgba(0,0,0,0)',
-                weight:2
-            }
-            
-            L.geoJSON(JSON.parse(data), {
-                style: basicStyle,
-                onEachFeature(feature,layer){
-                    layer.bindTooltip(feature.properties.nom_geo,{className:"toolTipHovered"});
-                    layer.on('click',function(e){
-                        $(".card").hide();
-                        $("#myInput").val("");
-                        $("#epci_"+feature.properties.code_geo).parent().show();
-                        $("#epci_"+feature.properties.code_geo).collapse("show");
-                     
-                    });
-                    layer.on(
-                        'mouseover', function(e){
-                            layer.setStyle(hoverStyle);
-                            layer.bringToFront();
-                            layer.openTooltip([e.latlng.lat,e.latlng.lng]);
-                        }
-                    );
-                    layer.on(
-                        'mouseout', function(e){
-                           layer.setStyle(basicStyle);
-                           layer.closeTooltip();
-                        }
-                    );
-                }
-            }).addTo(map);
-        },
-        error : function(){
-            console.log("GeoJson not loaded");
-        }
-    });
-   
     // Init List and Search
     var data = [];
     var template = "";
